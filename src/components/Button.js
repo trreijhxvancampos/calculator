479541be8c1d9f915d9f0587db02a4a99f2a7d62
R_Van <reijhx.campos@tooltwist.com>
2024-02-01 16:38:03 +0800

    added calculator features

---
+++ src/components/Button.js
@@ -0,0 +1,121 @@
+import { useContext } from "react"
+import { CalcContext } from "../context/CalcContext"
+
+const getStyleName = bttn => {
+  const className = {
+    '=': 'equals',
+    'x': 'opt',
+    '-': 'opt',
+    '+': 'opt',
+    '/': 'opt',
+
+  }
+  return className[bttn]
+}
+
+const Button = ({value}) => {
+  const {calc, setCalc} = useContext(CalcContext);
+  
+  // when user clicks comma
+  const commaClick = () => {
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes('.') ? calc.num + value:calc.num
+    })
+  }
+
+  // when user clicks C
+  const resetClick = () =>{
+    setCalc({ sign: '', num:0, res:0})
+  }
+
+  // when user clicks a number
+  const handleClickButton = () => {
+    const numberString = value.toString()
+
+    let numberValue;
+    if (numberString === '0' && calc.num ===0) {
+        numberValue = "0" // no need to increment additional 0 if num is 0
+    } else {
+      numberValue = Number(calc.num + numberString)
+    }
+    setCalc({
+      ...calc,
+      num: numberValue
+    })
+  }
+
+  // when user clicks operation
+  const signClick = () => {
+    setCalc({
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0
+    })
+  }
+
+  // when user clicks equals
+  const equalsClick = () => {
+    if (calc.res && calc.num) {
+      const math = (a, b, sign) => {
+        const result = {
+          '+': (a,b) => a+b,
+          '-': (a,b) => a-b,
+          'x': (a,b) => a*b,
+          '/': (a,b) => a/b,
+        }
+        return result[sign](a,b);
+      }
+      setCalc({
+        res:math(calc.res, calc.num, calc.sign),
+        sign: '',
+        num: 0
+      })
+    
+  }
+}
+  
+  // when user clicks percent
+  const percentClick = () => {
+    setCalc({
+      num: (calc.num / 100),
+      res: (calc.res / 100),
+      sign: ''
+    })
+  }
+
+  // when user clicks invert
+  const invertClick = () => {
+    setCalc({
+      num: calc.num ? calc.num *-1 : 0,
+      res: calc.res ? calc.res *-1 : 0,
+      sign: ''
+    })
+  }
+
+
+  const handleBttnClick = () => {
+    const results = {
+      '.': commaClick,
+      'C': resetClick,
+      '/': signClick,
+      'x': signClick,
+      '-': signClick,
+      '+': signClick,
+      '=': equalsClick,
+      // '%': percentClick,
+      // '+-': invertClick,
+    }  
+    if (results[value]) {
+      return results[value]()
+    } else{
+      return handleClickButton()
+    }
+  }
+
+  return (
+    <button className={`${getStyleName(value)} button`} onClick={handleBttnClick}>{value}</button>
+  )
+}
+
+export default Button
